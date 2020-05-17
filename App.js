13ee9f20d65e13b20c91c4bdb79da2440f6050d6
Ricardo Röhrs <rerohrs@inf.ufsm.br>
2020-05-17 19:39:06 -0300

    Imprime valores
    Lê e imprime um array de items em uma lista <li>

--- App.js
+++ App.js
@@ -1,103 +1,4 @@
-/* import React, { Component } from 'react';
-import { Text, Button, StyleSheet, Alert, View, FlatList, TextInput } from 'react-native'
-
-export default class App extends Component {
-  _onPressButton() {
-    alert('Arquivo selecionado!')
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
-
-  render() {
-    const dados = [
-      {key: 'Linha 1'},
-      {key: 'Linha 2'},
-      {key: 'Linha 3'},
-      {key: 'Linha 4'},
-      {key: 'Linha 5'}
-    ]
-
-    function rendertodo() {
-
-      for (todo of todos) {
-        var todoElement = document.createElement('li');
-        var todoText = document.createTextNode(todo);
-
-        todoElement.appendChild(todoText);
-        listElement.appendChild(todoElement);
-      }
-    }
-
-    function addtodo() {
-      var todoText = inputElement.value;
-
-      todos.push(todoText);
-      inputElement.value = '';
-      rendertodo();
-    }
-
-    return (
-      <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Upar arquivo!"
-          />
-        </View>
-
-      <View style={styles.input}>
-        <TextInput
-          placeholder="Escreva aqui!"
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        <Button
-            onPress={this._onPressButton}
-            title="Salvar valor"
-        />
-      </View>
-
-        <FlatList
-          data={dados}
-          renderItem = { ({item}) => <Text style={styles.textoItem}>{item.key}</Text> }
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-
-    container: {
-      flex: 1,
-      backgroundColor: "#f4f4f4"
-    },
-    input: {
-      margin: 5,
-      width: 250,
-      borderColor: "#000",
-      borderWidth: 2
-    },
-    textoItem: {
-      fontSize: 20,
-      color: "#000",
-      padding: 25,
-      borderBottomWidth: 2
-    },
-    buttonContainer: {
-      margin: 25
-    },
-    alternativeLayoutButtonContainer: {
-      margin: 20,
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    }
-}) */
-
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import { Text, Button, StyleSheet, Alert, View, FlatList, TextInput } from 'react-native'
 
 var SampleArray = ["ONE", "TWO"] ;
@@ -179,6 +80,98 @@ export default class MainActivity extends Component {
         <FlatList
           data={dados}
           renderItem = { ({item}) => <Text style={styles.textoItem}>{item.key}</Text> }
+        />
+      </View>
+   );
+ }
+}
+
+const styles = StyleSheet.create({
+
+  MainContainer: {
+    flex:1,
+    justifyContent: 'center',
+    backgroundColor: "#f4f4f4",
+    margin: 15
+  },
+  input: {
+    margin: 5,
+    width: 250,
+    borderColor: "#c3c3c3",
+    borderWidth: 1
+  },
+  textoItem: {
+    fontSize: 20,
+    color: "#000",
+    padding: 25,
+    borderBottomWidth: 2
+  },
+  buttonContainer: {
+    margin: 25
+  },
+});*/
+
+import React, { Component } from 'react';
+import { Text, Button, StyleSheet, Alert, View, FlatList, TextInput } from 'react-native'
+
+var SampleArray = [1, 2, 3, 4, 5];
+
+export default class MainActivity extends Component {
+  _onPressButton() {
+    alert('Arquivo selecionado!')
+  }
+
+  constructor(props) {
+       super(props)
+       this.state = {
+         Holder: ''
+       }
+  }
+
+  AddItemsToArray=()=>{
+      //Adding Items To Array.
+      SampleArray.push(this.state.Holder.toString());
+      // Showing the complete Array on Screen Using Alert.
+      Alert.alert(SampleArray.toString());
+  }
+
+   renderItem = ({item}) => (
+    <View>
+      <Text>{item}</Text>
+    </View>
+  );
+
+ render() {
+/* NÃO FUNCIONA  function rendertodo() {
+    for (todo of todos) {
+      var todoElement = document.createElement('li');
+      var todoText = document.createTextNode(todo);
+
+      todoElement.appendChild(todoText);
+      listElement.appendChild(todoElement);
+    }
+  }
+
+  function addtodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    rendertodo();
+  } NÃO FUNCIONA */
+   
+   return (
+      <View style={styles.MainContainer}>
+          <TextInput
+              placeholder="Digite o valor"
+              onChangeText={TextInputValue => this.setState({Holder: TextInputValue})}
+              style={{textAlign: 'center', marginBottom: 6, height: 45}}
+          />
+          <Button title="Adicionar valor" onPress={this.AddItemsToArray}/>
+
+        <FlatList
+          data={SampleArray}
+          renderItem = { ({item}) => <Text style={styles.textoItem}>{item}</Text>}
         />
       </View>
    );
