378f8abd49888b9c3f99296afba34df6131957e7
Ricardo Röhrs <rerohrs@inf.ufsm.br>
2020-05-31 20:51:26 -0300

    impressão dos dados
    Cria tabela completa, calcula e imprime na tela

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ var SampleArray = [1,2,10,3,4,5,6,7,8,9,0,1,5,2,3,6,4,9,7,8,1,2,3,4,6,9,5,2,3,1,
 ];
  */
 
-const numColumns = 6;
+/* const numColumns = 6;
 
 const formatData = (data, numColumns) => {
   const numberOfFullRows = Math.floor(data.length / numColumns);
@@ -27,92 +27,88 @@ const formatData = (data, numColumns) => {
   }
 
   return data;
-};
+}; */
 
 export default class MainActivity extends Component {
-  _onPressButton() {
-    alert('Arquivo selecionado!')
-  }
-
   constructor(props) {
        super(props)
        this.state = {
-        HeadTable: ['Dado', 'xi', 'Xi', 'fi', 'Fi', 'fr', 'Fr', '%'],
-        DataTable: [['Dado', 'xi', 'Xi', 'fi', 'Fi', 'fr', 'Fr', '%']],
+        HeadTable: ['Dado', 'xi', 'Xi', 'fr', 'Fr', '%'],
+        DataTable: [],
         Holder: ''
        }
   }
 
   AddItemsToArray = () => {
-      // Adding Items To Array.
       SampleArray.push(this.state.Holder.toString());
-      // Showing the complete Array on Screen Using Alert.
       //Alert.alert(SampleArray.toString());
   }
 
   calculate = () => {
     const data = SampleArray.sort((a,b)=>{
       return a - b;
-  });
+    });
+
     let k = Math.round((1 + 3.3 * Math.log10(data.length)));
     let max = Math.max(...data);
     let min = Math.min(...data);
     let A = max - min;
     let intervalo = (A/k).toFixed(4);
-
-    let inicio;
-    let final;
-    let intervalos = [];
-    
-    for (let i = 0; i<k; i++) {
-      inicio = min + i * intervalo;
-      final = min + (i+1) * intervalo;
-      intervalos.push([inicio,final]);
-    }
-
-    let xi = [0,0,0,0,0,0,0,0];
+    let xi = [0, 0, 0, 0, 0, 0, 0, 0];
     let intervaloAtual = 0;
     let ultimaIteracao = 0;
-
-     do {
-      for ( let i = ultimaIteracao; i<data.length; i++ ) {
-        if (i===0) {
-          if ( data[i] < intervalos[intervaloAtual][1])
-			      xi[intervaloAtual]++;
-		      else	{
-			      ultimaIteracao = i;
-			      intervaloAtual++;
-			      break;
-          }
-        } else if (i===data.length-1) {
-          if ( data[i] > intervalos[intervaloAtual][0] && data[i] <= intervalos[intervaloAtual][1])
-			      xi[intervaloAtual]++;
-		      else {
-			      ultimaIteracao = i;
-			      intervaloAtual++;
-			      break;
-          }
-        } else {
-          if ( data[i] >= intervalos[intervaloAtual][0] && data[i] < intervalos[intervaloAtual][1])
-			      xi[intervaloAtual]++;
-		      else {
-			      ultimaIteracao = i;
-			      intervaloAtual++;
-            break;
-          }
-        }
-      }
-    } while(intervaloAtual < intervalos.length);
-
+    let intervalos = [];
     let fr = [];
     let Xi = [];
     let Fr = [];
+    let percent = [];
+    let inicio;
+    let final;
+    let aux = [];
+    
+    for (let i = 0; i < k; i++) {
+      inicio = min + i * intervalo;
+      final = min + (i+1) * intervalo;
+      intervalos.push([inicio, final]);
+    }
+
+    do {
+    for ( let i = ultimaIteracao; i<data.length; i++ ) {
+      if (i===0) {
+        if ( data[i] < intervalos[intervaloAtual][1])
+          xi[intervaloAtual]++;
+        else	{
+          ultimaIteracao = i;
+          intervaloAtual++;
+          break;
+        }
+      } else if (i===data.length-1) {
+        if ( data[i] > intervalos[intervaloAtual][0] && data[i] <= intervalos[intervaloAtual][1])
+          xi[intervaloAtual]++;
+        else {
+          ultimaIteracao = i;
+          intervaloAtual++;
+          break;
+        }
+      } else {
+        if ( data[i] >= intervalos[intervaloAtual][0] && data[i] < intervalos[intervaloAtual][1])
+          xi[intervaloAtual]++;
+        else {
+          ultimaIteracao = i;
+          intervaloAtual++;
+          break;
+        }
+      }
+    }
+    } while(intervaloAtual < intervalos.length);
 
     for (let i = 0; i<xi.length; i++) {
       fr.push(xi[i]/data.length);
+      percent.push((fr[i])*100);
       if (i===0) {
         Xi.push(xi[i]);
         Fr.push(fr[i]);
+        Fr[i] = (fr[i]);
       } else {
         Xi.push(Xi[i-1] + xi[i]);
         Fr.push(Fr[i-1] + fr[i]);
@@ -120,34 +116,25 @@ export default class MainActivity extends Component {
     }
 
     console.log('   ');
+    console.log('xi', xi);
     console.log('Xi', Xi);
     console.log('fr', fr);
     console.log('Fr', Fr);
-/*     const localDataTable = this.state.DataTable;
+    console.log('%', percent);
+    console.log(intervalos); 
 
-    for (let i = 0; i<intervalos; i++) {
-      let aux = intervalos[i][0] + '---' + intervalos[i][1];
-      localDataTable.push([aux,0,0,0,0,0,0,0]);
+    for (let i = 0; i<k; i++) {
+      intervalos[i].splice(1, 0, " ... ");
     }
 
-    this.setState({DataTable: localDataTable}); */
-    
+    let array = [];
 
-/*     intervalos.forEach((item)=>{
-      let aux = item[0] + '---' + item[1];
-      this.state.DataTable.push([aux,0,0,0,0,0,0,0]);
-    }); */
-    
-   console.log(intervalos); 
-    
-//    Alert.alert(intervalo.toString());  
+    for (let i=0; i<xi.length; i++) {
+      array.push([intervalos[i], xi[i], Xi[i], fr[i].toFixed(4), Fr[i].toFixed(4), percent[i].toFixed(2)])
+    }
+
+    this.setState({DataTable: array});
   }
-
-/*    renderItem = ({item}) => (
-    <View>
-      <Text>{item}</Text>
-    </View>
-  ); */
 
   renderItem = ({ item }) => {
     if (item.empty === true) {
@@ -187,7 +174,12 @@ export default class MainActivity extends Component {
 
         <Table borderStyle={{borderWidth: 1, borderColor: '#333'}}>
           <Row data={state.HeadTable}/>
-          <Rows data={state.DataTable}/>
+          {this.state.DataTable.map((rowData, index) => (
+            <Row
+              key={index}
+              data={rowData}
+            />
+          ))}
         </Table>
 
       </View>
